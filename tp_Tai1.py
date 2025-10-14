0c11cea18b3673cadc09834a8def80b3990792d2
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2025-10-14 12:10:45 +0100

    tp2

--- tp_Tai1.py
+++ tp_Tai1.py
@@ -6,29 +6,34 @@ img_path = "lena.jpg"
 
 image_color=cv2.imread(img_path,cv2.IMREAD_COLOR)
 img_rgb=cv2.cvtColor(image_color,cv2.COLOR_BGR2RGB)
+img_gray = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-b,g,r=cv2.split(img_rgb)
+Img_echantillon = cv2.resize(img_gray, (50, 50), interpolation=cv2.INTER_NEAREST)
 
-plt.figure(figsize=(10,4))
 
-plt.subplot(1,3,1)
 
-plt.title("Canal Rouge")
 
-plt.imshow(r, cmap="gray")
-
-plt.subplot(1,3,2)
-
-plt.title("Canal vert")
-
-plt.imshow(g, cmap="gray")
-
-plt.subplot(1,3,3)
-
-plt.imshow(b, cmap="gray")
-
-plt.title("Canal bleu")
-
+plt.figure(figsize=(5,5))
+plt.title("Lena sous-échantillonnée")
+plt.imshow(Img_echantillon, cmap='gray')
+plt.axis('off')
 plt.show()
+
+
+
+def _imgquantif(img,k):
+  step=256 //k
+  quantized = (img // step) * step + step // 2
+  quantized = np.clip(quantized, 0, 255)  
+  return quantized.astype(np.uint8)
+
+
+
+quant_img = _imgquantif(img_gray, 4)
+plt.imshow(quant_img, cmap='gray')
+
+plt.suptitle("Quantification de l'image ")
+plt.show()
+
 
 
